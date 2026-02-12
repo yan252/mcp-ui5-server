3e49ef32eee0d75778e1198807fbb1905c8af6be
Gianluca Beil <66752411+werwolf2303@users.noreply.github.com>
2026-02-12 16:03:31 +0100

    ci: Replace depcheck with knip (#223)
    Due to the deprecation of depcheck we migrated to knip, which is in active development and has more features. As a first step we only activate the checking of unused dependencies.
    
    JIRA: CPOUI5FOUNDATION-1126

---
+++ knip.config.js
@@ -0,0 +1,28 @@
+const config = {
+	/**
+	 * We only need dependency checking at the moment,
+	 * so all checks except for dependencies are turned off.
+	 */
+	rules: {
+		files: "off",
+		duplicates: "off",
+		classMembers: "off",
+		unlisted: "off",
+		binaries: "off",
+		unresolved: "off",
+		catalog: "off",
+		exports: "off",
+		types: "off",
+		enumMembers: "off",
+	},
+
+	ignoreDependencies: [
+		/**
+		 * Used via nyc ava --node-arguments="--experimental-loader=@istanbuljs/esm-loader-hook"
+		 * which is not detected by knip as a usage of this package
+		 */
+		"@istanbuljs/esm-loader-hook",
+	],
+};
+
+export default config;
